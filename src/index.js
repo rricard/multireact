54eddfa1966511275096a7c433ab7279548f8c8f
Robin Ricard <ricard.robin@gmail.com>
2017-06-21 19:18:45 +0200

    Re-enable SW

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import App from './App';
 import { AppRegistry } from 'react-native';
+import registerServiceWorker from './registerServiceWorker';
 
 AppRegistry.registerComponent('MyApp', () => App);
 AppRegistry.runApplication('MyApp', { rootTag: document.getElementById('root') });
+registerServiceWorker();
